e37144d44a350f28f77255d682457bf8d7fd23f1
William Forde <willforde@gmail.com>
2017-04-07 22:24:24 +0100

    correct sys path

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # Fetch version number
 from urlquick import __version__, __copyright__, __author__
