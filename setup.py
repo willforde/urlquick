5ef6df95de427f9891a5b927d1c8b62a9f62d5c3
William Forde <willforde@gmail.com>
2017-04-14 05:32:16 +0100

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 
 setup(name='urlquick',
-      version='0.1.0',
+      version='0.1.1',
       description='A light-weight http client with requests like interface. Featuring persistent connections and caching support.',
       long_description=readme(),
       keywords='url lightweight caching http-client requests',
--- urlquick.py
+++ urlquick.py
@@ -109,7 +109,7 @@ __repo__ = "https://github.com/willforde/urlquick"
 __copyright__ = "Copyright (C) 2017 William Forde"
 __author__ = "William Forde"
 __license__ = "GPLv3"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Cacheable request types
 CACHEABLE_METHODS = (u"GET", u"HEAD", u"POST")
