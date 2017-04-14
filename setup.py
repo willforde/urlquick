220f80e0a8ee3244cc663c030c73d40341b1c36e
William Forde <willforde@gmail.com>
2017-04-14 15:01:17 +0100

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 
 setup(name='urlquick',
-      version='0.1.1',
+      version='0.1.2',
       description='A light-weight http client with requests like interface. Featuring persistent connections and caching support.',
       long_description=readme(),
       keywords='url lightweight caching http-client requests',
