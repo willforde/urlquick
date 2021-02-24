cd40786441e0cf8ae617751e2e75c4c4995cd965
willforde <willforde@gmail.com>
2021-02-24 16:11:52 +0000

    test changes

--- tests/test_session.py
+++ tests/test_session.py
@@ -5,4 +5,5 @@ def test_get(requests_mock):
     requests_mock.get('https://www.test.com', text="data")
     ret = urlquick.get('https://www.test.com')
     print(type(ret.content), ret.content)
-    assert ret.content == "data"
+    assert ret.content == b"data"
+    assert ret.text == "data"
